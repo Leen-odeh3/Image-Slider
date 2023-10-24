57ba1fba38e2ba54a08f0cd5c9984eb22722de6d
leen <leenodeh287@gmail.com>
2023-10-24 10:28:10 +0300

    adding styles

--- src/Component/ImageSlider.jsx
+++ src/Component/ImageSlider.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Data } from "../data/data";
+import "./ImageSlider.css";
 
 const ImageSlider = () => {
   return (
-    <div>
-      
+    <div className="container">
+      <div className="main">
+        <i class="bi bi-arrow-right-circle"></i>
+        <div className="wrapper">
+          {Data.map((e, index) => (
+            <div key={index} className="content">
+              <img src={e.image} alt={`slide${e.slide}`} width="100%" />
+            </div>
+          ))}
+        </div>
+        <i class="bi bi-arrow-left-circle"></i>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ImageSlider
+export default ImageSlider;

9631adb51cf348792790541a753e1be1fdcfb0ca
leen <leenodeh287@gmail.com>
2023-10-24 10:54:13 +0300

    done task

--- src/Component/ImageSlider.jsx
+++ src/Component/ImageSlider.jsx
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Data } from "../data/data";
 import "./ImageSlider.css";
 
 const ImageSlider = () => {
+  const [index, setIndex] = useState(0);
+
+  const handleNext = () => {
+    setIndex((prevIndex) => (prevIndex + 1) % Data.length);
+  };
+
+  const handlePrev = () => {
+    setIndex((prevIndex) => (prevIndex - 1 + Data.length) % Data.length);
+  };
+
+  const slideStyle = {
+    transform: `translateX(-${index * 100}%)`
+  };
+
   return (
     <div className="container">
       <div className="main">
-        <i class="bi bi-arrow-right-circle"></i>
-        <div className="wrapper">
-          {Data.map((e, index) => (
-            <div key={index} className="content">
+        <i className="bi bi-arrow-right-circle" onClick={handleNext}></i>
+        <div className="wrapper" style={slideStyle}>
+          {Data.map((e, idx) => (
+            <div key={idx} className="content">
               <img src={e.image} alt={`slide${e.slide}`} width="100%" />
             </div>
           ))}
         </div>
-        <i class="bi bi-arrow-left-circle"></i>
+        <i className="bi bi-arrow-left-circle" onClick={handlePrev}></i>
       </div>
     </div>
   );
